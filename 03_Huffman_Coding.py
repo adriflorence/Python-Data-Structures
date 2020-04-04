921cc7f0d3cfc81f77f39a6958d16b4556f86fdb
Adri Forczek <forczekadri@gmail.com>
2020-04-04 22:21:08 +0100

    chore: returns character frequency in a string

--- 03_Huffman_Coding.py
+++ 03_Huffman_Coding.py
@@ -1,8 +1,27 @@
 import sys
 
+# Take a string and determine the relevant frequencies of the characters.
+# Build and sort a list of tuples from lowest to highest frequencies.
+# Build the Huffman Tree by assigning a binary code to each letter, using shorter codes for the more frequent letters. (This is the heart of the Huffman algorithm.)
+# Trim the Huffman Tree (remove the frequencies from the previously built tree).
+
+def get_frequencies(data):
+    char_dict = {}
+    for char in data:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else: 
+            char_dict[char] += 1
+    #create a sorted list key, freq tuple pairs
+    sorted_frequencies = sorted(zip(char_dict.values(), char_dict.keys()))
+    print(sorted_frequencies)
+
+# Encode the text into its compressed form.
 def huffman_encoding(data):
+    get_frequencies(data)
     pass
 
+# Decode the text from its compressed form.
 def huffman_decoding(data,tree):
     pass
 
